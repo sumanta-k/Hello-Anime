997fa493be22b3e4acd3c8f295ca8108d59f117b
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-05-01 09:52:10 +0530

    feat: add base form of Hello-Anime.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const GetTitle = () => {
+    return (
+        <div>
+            <h1>Hello Anime</h1>
+        </div>
+    );
+};
+function App() {
+    return <GetTitle />;
+}
+
+export default App;
